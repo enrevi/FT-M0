40f4d20ae21fb81083c96fbaf232c79feddd1032
Enrique Reyes <enrevi@hotmail.com>
2021-06-30 14:39:09 -0500

    primer clase js

---
+++ 02-JS-I/homework/tests/test1.js
@@ -0,0 +1,11 @@
+var nombre= "Enrique";
+let apellido= "Reyes";
+var edad= 42;
+const humano= true
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(humano);
+
+console.log("Hello World");
